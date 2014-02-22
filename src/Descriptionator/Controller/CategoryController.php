5b407af9f580fd2300594f72f2e19ec4b6963415
aude <aude.wiki@gmail.com>
2014-02-22 15:20:37 +0100

    improve category controller

--- src/Descriptionator/Controller/CategoryController.php
+++ src/Descriptionator/Controller/CategoryController.php
@@ -31,23 +31,49 @@ class CategoryController implements ControllerProviderInterface {
 		$itemLookup = new ItemApiLookup( $repo );
 		$items = $itemLookup->getItemsBySiteLinks( $pages, 'enwiki' );
 
+		$itemList = $this->buildItemList( $items );
+		$itemData = $this->buildItemData( $itemList );
+
+		return $app['twig']->render(
+			'category_list.twig',
+			array(
+				'pages' => array(),
+				'items' => $itemData
+			)
+		);
+	}
+
+	private function buildItemList( $items ) {
 		$itemList = array();
-		$missing = array();
 
 		$deserializer = new ItemDeserializer();
 
 		foreach( $items as $item ) {
-			if ( array_key_exists( 'missing', $item ) ) {
-				$missing[] = $item;
-			} else {
+			if ( !array_key_exists( 'missing', $item ) ) {
 				$itemList[] = $deserializer->deserialize( $item );
 			}
 		}
 
-		$formattedItems = array();
+		return $itemList;
+	}
 
-		foreach( $itemList as $item ) {
-			$formattedItems[] = array(
+	private function buildMissingList( $items ) {
+		$missing = array();
+
+		foreach( $items as $item ) {
+			if ( array_key_exists( 'missing', $item ) ) {
+				$missing[] = $item;
+			}
+		}
+
+		return $missing;
+	}
+
+	private function buildItemData( array $items ) {
+		$itemData = array();
+
+		foreach( $items as $item ) {
+			$itemData[] = array(
 				'id' => $item->getId()->getSerialization(),
 				'page' => $item->getSiteLink( 'enwiki' )->getPageName(),
 				'label' => $item->getLabel( 'en' ),
@@ -55,13 +81,7 @@ class CategoryController implements ControllerProviderInterface {
 			);
 		}
 
-		return $app['twig']->render(
-			'category_list.twig',
-			array(
-				'pages' => array(),
-				'items' => $formattedItems
-			)
-		);
+		return $itemData;
 	}
 
 }
