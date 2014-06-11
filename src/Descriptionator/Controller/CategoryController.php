014ce86155e302f1236721a83163e3dc47e0f90b
aude <aude.wiki@gmail.com>
2014-06-11 06:41:11 +0200

    add membersWithNoDescription for CategoryController

--- src/Descriptionator/Controller/CategoryController.php
+++ src/Descriptionator/Controller/CategoryController.php
@@ -3,8 +3,6 @@
 namespace Descriptionator\Controller;
 
 use Descriptionator\MediaWiki\CategoryMemberApiLookup;
-use Descriptionator\MediaWiki\Wiki;
-use Descriptionator\Wikidata\ItemApiLookup;
 use Descriptionator\Wikidata\ItemDeserializer;
 use Silex\Application;
 use Silex\ControllerProviderInterface;
@@ -12,8 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Wikibot\CategoryMemberFinder;
 use Wikibot\Wikibase\ItemLookup;
 use Wikibot\Wikibase\ItemsByCategoryFinder;
-use WikiClient\MediaWiki\User;
-use WikiClient\MediaWiki\WikiFactory;
+use Wikibot\Wikibase\ItemsWithoutDescriptionFinder;
 
 class CategoryController implements ControllerProviderInterface {
 
@@ -21,19 +18,24 @@ class CategoryController implements ControllerProviderInterface {
 		$controller = $app['controllers_factory'];
 
 		$controller->get( '/{catname}/', array( $this, 'members' ) );
+		$controller->get( '/{catname}/nodesc/', array( $this, 'membersWithNoDescription' ) );
 
 		return $controller;
 	}
 
 	public function members( Application $app, $catname ) {
-		$client = $app['apiclient'];
+		$items = $this->getItemsForCategory( $app, $catname );
 
-		$catMemberFinder = new CategoryMemberFinder( $client( 'enwiki' ) );
-		$itemLookup = new ItemLookup( $client( 'wikidatawiki' ), $app['entity-deserializer'] );
+		return $this->buildRendering( $app, $items );
+	}
 
-		$itemsByCatFinder = new ItemsByCategoryFinder( $catMemberFinder, $itemLookup );
-		$items = $itemsByCatFinder->getItemsForCategory( $catname );
+	public function membersWithNoDescription( Application $app, $catname ) {
+		$items = $this->getItemsWithNoDescription( $app, $catname );
 
+		return $this->buildRendering( $app, $items );
+	}
+
+	private function buildRendering( $app, array $items ) {
 		$itemData = $this->buildItemData( $items );
 
 		return $app['twig']->render(
@@ -43,6 +45,29 @@ class CategoryController implements ControllerProviderInterface {
 				'items' => $itemData
 			)
 		);
+	}
+
+	private function getItemsWithNoDescription( $app, $catname ) {
+		$client = $app['apiclient'];
+
+		$itemLookup = new ItemLookup( $client( 'wikidatawiki' ), $app['entity-deserializer'] );
+		$itemFinder = new ItemsWithoutDescriptionFinder( $client( 'enwiki' ), $itemLookup );
+
+		$items = $itemFinder->getItems( $catname, 'enwiki', 'en' );
+
+		return $items;
+	}
+
+	private function getItemsForCategory( $app, $catname ) {
+		$client = $app['apiclient'];
+
+		$catMemberFinder = new CategoryMemberFinder( $client( 'enwiki' ) );
+		$itemLookup = new ItemLookup( $client( 'wikidatawiki' ), $app['entity-deserializer'] );
+
+		$itemsByCatFinder = new ItemsByCategoryFinder( $catMemberFinder, $itemLookup );
+		$items = $itemsByCatFinder->getItemsForCategory( $catname );
+
+		return $items;
 	}
 
 	private function buildItemData( array $items ) {
